22400673410b1538f63cb50415b082191303e7f5
Pin Chennn <pin50195.pc@gmail.com>
2024-06-27 16:19:38 +0800

    commit

--- src/component/register-component.js
+++ src/component/register-component.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AuthService from "../services/auth.service";
+import Loading from "./Loading";
 
 const RegisterComponent = ({
   currentUser,
